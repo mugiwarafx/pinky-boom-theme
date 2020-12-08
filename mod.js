54adec26085981eb131104d61b528381bc118ced
mugiwarafx <58401248+mugiwarafx@users.noreply.github.com>
2020-12-08 20:30:58 +0100

    pinky-boom-dark 0.0.1

--- mod.js
+++ mod.js
@@ -10,7 +10,7 @@ module.exports = {
     id: "fbef391c-58ff-4938-bd45-b85ae0435e4e",
     name: "Pinky Boom Theme",
     tags: ['theme', 'dark', 'pink'],
-    desc: "soft pinkify your dark theme",
+    desc: "pinkify your dark theme",
     version: "0.0.1",
     author: "mugiwarafx",
 };
