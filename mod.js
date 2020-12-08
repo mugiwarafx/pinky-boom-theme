16d8c70bbb4ea0db76a4a1c97ecd0926a314f40d
mugiwarafx <58401248+mugiwarafx@users.noreply.github.com>
2020-12-08 17:16:36 +0100

    update mod.js theme description

--- mod.js
+++ mod.js
@@ -1,5 +1,5 @@
 /*
- * Pinky Booooom Theme! ["soft" boom 😂]
+ * Pinky Booooom Theme! ["soft" pinky boom 😂]
  * (c) 2020 mugiwarafx (https://github.com/mugiwarafx)
  * under the MIT license
  */
@@ -9,8 +9,8 @@
 module.exports = {
     id: "fbef391c-58ff-4938-bd45-b85ae0435e4e",
     name: "Pinky Boom Theme",
-    tags: ['theme', 'pink'],
-    desc: "Soft pinkify your light theme",
+    tags: ['theme', 'light', 'pink'],
+    desc: "soft pinkify your light theme",
     version: "0.0.1",
     author: "mugiwarafx",
 };
