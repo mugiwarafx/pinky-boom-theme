fee901d3b91695d8ae2c68e1fbea8be2ae76d5e7
mugiwarafx <58401248+mugiwarafx@users.noreply.github.com>
2020-12-08 17:23:09 +0100

    pinky-boom-light 0.0.1

--- mod.js
+++ mod.js
@@ -9,8 +9,8 @@
 module.exports = {
     id: "fbef391c-58ff-4938-bd45-b85ae0435e4e",
     name: "Pinky Boom Theme",
-    tags: ['theme', 'pink'],
-    desc: "Pinky everywhere and black cats",
+    tags: ['theme', 'light', 'pink'],
+    desc: "pinkify your light theme",
     version: "0.0.1",
     author: "mugiwarafx",
 };
