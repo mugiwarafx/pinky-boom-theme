0f8052d8a29a5cd1c03410dca045e8b2e097918d
mugiwarafx <58401248+mugiwarafx@users.noreply.github.com>
2020-12-08 17:17:51 +0100

    soft-boom-dark 0.0.1

--- mod.js
+++ mod.js
@@ -1,5 +1,5 @@
 /*
- * Pinky Booooom Theme!
+ * Pinky Booooom Theme! ["soft" pinky boom 😂]
  * (c) 2020 mugiwarafx (https://github.com/mugiwarafx)
  * under the MIT license
  */
@@ -9,8 +9,8 @@
 module.exports = {
     id: "fbef391c-58ff-4938-bd45-b85ae0435e4e",
     name: "Pinky Boom Theme",
-    tags: ['theme', 'pink'],
-    desc: "Pinky everywhere and black cats",
+    tags: ['theme', 'dark', 'pink'],
+    desc: "soft pinkify your dark theme",
     version: "0.0.1",
     author: "mugiwarafx",
 };
